e7d8f1830068e8bd8304e26fc243bd03a0fbdcb8
Tokyo <ahmed.tokyo1@gmail.com>
2017-06-06 02:44:29 +0200

    basic functionality

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+Main();
+
+function Main() {
+  let package = null;
+  try {
+    package = require(path.join(process.cwd(), 'package.json'));
+  } catch (e) {
+    console.log('Error reading package.json');
+    console.log(e);
+    return;
+  }
+
+  let fileText = '# Dependancies: \n';
+  fileText += packagesToFileText(package.dependencies);
+
+  fileText += '# Dev Dependancies: \n';
+  fileText += packagesToFileText(package.devDependencies);
+
+  fs.writeFileSync(path.join(process.cwd(), 'dependencies.md'), fileText);
+}
+
+function packagesToFileText (dependencies) {
+  const keysArray = Object.keys(dependencies);
+  let text = '';
+  for (let i = 0; i < keysArray.length; i++) {
+    const line = `Package Name: ${keysArray[i]}, Package Url: https://www.npmjs.com/package/${keysArray[i]}, Version: ${dependencies[keysArray[i]]} \n`;
+    text += line;
+  }
+  return text;
+}
